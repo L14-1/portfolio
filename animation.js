e9faa6fa75c90b6f812441d0fcfc744c8fe00edb
L14-1 <nico.maitre.stair@gmail.com>
2021-10-01 01:36:42 +0200

    adding animation

---
+++ animation.js
@@ -0,0 +1,37 @@
+// let circle1 = document.getElementById('c1')
+// let circle2 = document.getElementById('c2')
+// let circle3 = document.getElementById('c3')
+// let circle4 = document.getElementById('c4')
+// let circle5 = document.getElementById('c5')
+
+// let delay = 1500;
+
+// function anim() {
+//     circle4.className = 'circle c4 topLeft'
+//     console.log('lol1');
+//     setTimeout(function() {
+//         console.log('lol2');
+//         circle4.className = 'circle c4 top'
+//       }, delay);
+//       setTimeout(function() {
+//         circle4.className = 'circle c4 topRight'
+//         console.log('lol3');
+//       }, delay);
+//       setTimeout(function() {
+//         circle4.className = 'circle c4'
+//         console.log('lol4');
+//       }, delay);
+// }
+
+// anim()
+// anim()
+// anim()
+// anim()
+// anim()
+// anim()
+// anim()
+// anim()
+// anim()
+// anim()
+// anim()
+// anim()
