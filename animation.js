3631b855f00912e36d359191a6b6874d14fc86fe
L14-1 <nico.maitre.stair@gmail.com>
2021-10-04 13:27:27 +0200

    social appearence and adding h2

--- animation.js
+++ animation.js
@@ -1,37 +1,13 @@
-// let circle1 = document.getElementById('c1')
-// let circle2 = document.getElementById('c2')
-// let circle3 = document.getElementById('c3')
-// let circle4 = document.getElementById('c4')
-// let circle5 = document.getElementById('c5')
+let footer = document.getElementById('footer')
+let contactText = document.getElementById('contactText')
+let social = document.getElementById('social')
 
-// let delay = 1500;
+footer.addEventListener("mouseover", function () {
+    contactText.className = ('topP')
+    social.className = ('social socialAppears')
+})
 
-// function anim() {
-//     circle4.className = 'circle c4 topLeft'
-//     console.log('lol1');
-//     setTimeout(function() {
-//         console.log('lol2');
-//         circle4.className = 'circle c4 top'
-//       }, delay);
-//       setTimeout(function() {
-//         circle4.className = 'circle c4 topRight'
-//         console.log('lol3');
-//       }, delay);
-//       setTimeout(function() {
-//         circle4.className = 'circle c4'
-//         console.log('lol4');
-//       }, delay);
-// }
-
-// anim()
-// anim()
-// anim()
-// anim()
-// anim()
-// anim()
-// anim()
-// anim()
-// anim()
-// anim()
-// anim()
-// anim()
+footer.addEventListener("mouseout", function () {
+    contactText.className = ('')
+    social.className = ('social')
+})
