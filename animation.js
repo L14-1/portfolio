27fb4f2be008157759240bcbb47224a187dc4e9d
L14-1 <nico.maitre.stair@gmail.com>
2021-10-04 14:02:32 +0200

    animation projets

--- animation.js
+++ animation.js
@@ -1,3 +1,5 @@
+// Animation apparition social
+
 let footer = document.getElementById('footer')
 let contactText = document.getElementById('contactText')
 let social = document.getElementById('social')
@@ -11,3 +13,52 @@ footer.addEventListener("mouseout", function () {
     contactText.className = ('')
     social.className = ('social')
 })
+
+// Animation apparition des projets
+
+
+window.addEventListener('scroll', function () {
+        let project = document.getElementsByClassName('project');
+    
+        project = project[0]
+
+        let projectPosition = project.getBoundingClientRect().top;
+        let screenPosition = window.innerHeight / 2.5;
+
+        if (projectPosition < screenPosition) {
+            project.classList.add('active');
+        } else {
+            project.classList.remove('active');
+        }
+})
+
+window.addEventListener('scroll', function () {
+    let project = document.getElementsByClassName('project');
+
+    project = project[1]
+
+    let projectPosition = project.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight / 2.3;
+
+    if (projectPosition < screenPosition) {
+        project.classList.add('active');
+    } else {
+        project.classList.remove('active');
+    }
+})
+
+window.addEventListener('scroll', function () {
+    let project = document.getElementsByClassName('project');
+
+    project = project[2]
+
+    let projectPosition = project.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight / 2.5;
+
+    if (projectPosition < screenPosition) {
+        project.classList.add('active');
+    } else {
+        project.classList.remove('active');
+    }
+})
+
