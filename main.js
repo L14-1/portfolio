d53e5c1ca239f0296e3ff1d2a3a2040b2383c5a0
l14-1 <nico.maitre.stair@gmail.com>
2022-05-10 11:33:18 +0200

    updating exp section

--- main.js
+++ main.js
@@ -12,17 +12,3 @@ var scrollCn = function(quantity)
 };
 window.addEventListener('scroll', function(){ scrollCn(10) });
 
-// window.onscroll = function() {
-//     scrollFunction()
-//   };
-
-// function scrollFunction() {
-//     let appearance = window.innerHeight / 2;
-//     if (document.body.scrollTop > appearance || document.documentElement.scrollTop > appearance) {
-//       document.getElementById("links").style.fontSize = "1.3rem";
-//     //   document.getElementById("links").style.borderBottom = "1px solid #fff";
-//     } else {
-//       document.getElementById("links").style.fontSize = "0";
-//     //   document.getElementById("links").style.borderBottom = "none";
-//     }
-// }
